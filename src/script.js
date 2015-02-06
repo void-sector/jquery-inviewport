c333de84d7e0920aed9f130fa5c0fa4651996b1d
void-sector <voidsector@example.com>
2015-02-06 16:18:25 +0100

    testing a new idea

--- src/script.js
+++ src/script.js
@@ -5,48 +5,26 @@
  * @returns {undefined}
  */
 ;(function($) {
-    $.fn.isInView = function(callback) {
+     $.fn.loadCkeditor = function() {
         "use strict";
-        
-        this.each(function() {
-            var rect = this.getBoundingClientRect();
 
-            if (rect.top >= 0 && rect.top < (window.innerHeight || document.documentElement.clientHeight) ||
-                rect.bottom >= 0 && rect.bottom < (window.innerHeight || document.documentElement.clientHeight)  ) {
-                
-                // call the callback!!
-                callback(
-                    $(this)
-                );
-            }
-        });
- 
-        return $(this);            
+        // load the ckeditor if it is not dont yet
+        if (this.hasClass('ckeditorLazyLoad')) {
+            this.removeClass('ckeditorLazyLoad');
+            CKEDITOR.replace(this.attr('id'));
+        }
     };
- 
+    
+    $.extend($.expr[':'], {
+        "in-viewport": function(element, index, meta) {
+            var rect = element.getBoundingClientRect();
+            return (
+                rect.top >= 0 && rect.top < (window.innerHeight || document.documentElement.clientHeight) ||
+                rect.bottom >= 0 && rect.bottom < (window.innerHeight || document.documentElement.clientHeight)
+            );
+        }
+    });
 }(jQuery));
-
-
-
-
-
-/**
- * callback function
- * 
- * @todo find out how to pass the jquery element without using a parameter
- * @param {type} el
- * @returns {undefined}
- */
-var loadCKEditor = function(el) {
-    "use strict";
-
-    // load the ckeditor if it is not dont yet
-    if (el.hasClass('ckeditorLazyLoad')) {
-        el.removeClass('ckeditorLazyLoad');
-        CKEDITOR.replace(el.attr('id'));
-    }
-};
-
 
 
 
@@ -58,10 +36,11 @@ var loadCKEditor = function(el) {
  */
 ;$(function() {
     "use strict";
-
-    $(".ckeditorLazyLoad").isInView(loadCKEditor);
-    
     $(window).bind('scroll', function() {
-        $(".ckeditorLazyLoad").isInView(loadCKEditor);
+        $(".ckeditorLazyLoad:in-viewport").loadCkeditor({
+            config: null,
+            width: null,
+            height: null
+        }); //css({"background-color":"#f00"});
     });
 });
